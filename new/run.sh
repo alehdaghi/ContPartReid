cc85605a1c661abfb79989d9e2549a97f894eeb3
Rajarshi Bhattacharya <rajbhogthesecond@gmail.com>
2024-10-12 15:34:16 -0400

    script changes

--- new/run.sh
+++ new/run.sh
@@ -1,1 +1,3 @@
-python train.py --log_name=test2.txt
+#python train.py --log_name=test2.txt
+#python test.py --resume='./checkpoints/sysu/SYSU_log.txt/model_best.pth'
+python test.py --resume='/export/livia/home/vision/Rbhattacharya/work/MixVI-ReID/SAAI/checkpoints/sysu/SYSU_test_og/model_best.pth'
