9de7035fc807c6a417b5f032e405266f8fa9dac6
Rajarshi Bhattacharya <rajbhogthesecond@gmail.com>
2024-11-04 10:07:31 -0500

    added run script for vit

--- new/run.sh
+++ new/run.sh
@@ -1,3 +1,3 @@
-#python train.py --log_name=test2.txt
+python train.py --log_name=test_setup.txt --backbone=vit_b_16
 #python test.py --resume='./checkpoints/sysu/SYSU_log.txt/model_best.pth'
-python test.py --resume='/export/livia/home/vision/Rbhattacharya/work/MixVI-ReID/SAAI/checkpoints/sysu/SYSU_test_og/model_best.pth'
+#python test.py --resume='/export/livia/home/vision/Rbhattacharya/work/MixVI-ReID/SAAI/checkpoints/sysu/SYSU_test_og/model_best.pth'
